2093b05c0325dbef75447a3756276695bc26b718
Ramon Limas <ramon.limas@sistemainfo.com.br>
2020-05-19 00:22:39 -0300

    mask number

--- src/app/lancamentos/lancamentos.module.ts
+++ src/app/lancamentos/lancamentos.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { LancamentosPageRoutingModule } from './lancamentos-routing.module';
-
 import { LancamentosPage } from './lancamentos.page';
+import {NgxMaskIonicModule} from 'ngx-mask-ionic';
+
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    NgxMaskIonicModule,
     LancamentosPageRoutingModule
   ],
   declarations: [LancamentosPage]
